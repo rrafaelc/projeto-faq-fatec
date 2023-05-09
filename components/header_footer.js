7520f75f2c3a08116e687cd46a65f647b73fa292
Rafael Costa <rafaelcosta8020@gmail.com>
2023-05-09 20:12:32 -0300

    test

--- components/header_footer.js
+++ components/header_footer.js
@@ -3,7 +3,27 @@
 // Usar esse componente para carregar o header e o footer em todas as paginas
 // sem precisar ficar copiando e colando muito
 
-document.getElementById('header').innerHTML = `<p>Cabecalho</p>`;
+document.getElementById('header').innerHTML = `
+<div class="header-content">
+<div class="logo">
+  <img
+    src="../img/fatec_ra_campinas_itapira_cor.png"
+    alt="Logo da fatec"
+  />
+</div>
+<div class="nav-items">
+  <a href="/" class="active">Página inicial</a>
+  <a href="">Mais buscados</a>
+  <a href="">Sugestões</a>
+  <a href="">Sobre</a>
+</div>
+<div class="login">
+  <a href="/login">
+    <p>Login</p>
+    <p>(Somente autorizados)</p>
+  </a>
+</div>
+</div>
+`;
 // document.getElementById('navbar').innerHTML = `<p>Menu</p>`;
 document.getElementById('footer').innerHTML = `<p>Rodape</p>`;
-
