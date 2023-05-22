fa2ba949e06dd26cedd443318c431eb1bb49f26b
Rafael Costa <rafaelcosta8020@gmail.com>
2023-05-22 19:34:51 -0300

    fix

--- scripts/global.js
+++ scripts/global.js
@@ -12,26 +12,3 @@ const hamburgerContent = document.querySelector('.menu-hamburger-content');
 hamburgerIcon.addEventListener('click', function () {
   hamburgerContent.classList.toggle('show');
 });
-
-// deixa o link vermelho quando acessa a pagina dele
-
-const urlDaPagina = window.location.pathname;
-const paginaInicial = document.querySelector('.nav-items :nth-child(1)');
-const maisBuscados = document.querySelector('.nav-items :nth-child(2)');
-const sugestoes = document.querySelector('.nav-items :nth-child(3)');
-const sobre = document.querySelector('.nav-items :nth-child(4)');
-const areaRestrita = document.querySelectorAll('.area-r');
-
-if (urlDaPagina.includes('/mais-buscados')) {
-  maisBuscados.classList.add('.active');
-} else if (urlDaPagina.includes('/sugestoes')) {
-  sugestoes.classList.add('active');
-} else if (urlDaPagina.includes('/sobre')) {
-  sobre.classList.add('active');
-} else if (urlDaPagina.includes('/login')) {
-  areaRestrita.forEach(a => {
-    a.classList.add('active');
-  });
-} else {
-  paginaInicial.classList.add('active');
-}
