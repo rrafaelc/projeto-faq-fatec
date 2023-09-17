3d14a9d610e395592a374a50ef0b17b889fb039d
Rafael Costa <rafaelcosta8020@gmail.com>
2023-09-17 12:05:30 -0300

    feat: adicionado header dinamico para todas as paginas

---
+++ layouts/header.php
@@ -0,0 +1,29 @@
+<header>
+  <div class="header-content">
+    <div class="logo">
+      <a href=<?= "$dir/" ?>><img src=<?= "$dir/img/fatec_ra_campinas_itapira_cor.svg"  ?> alt="Logo da fatec" /></a>
+    </div>
+    <div class="nav-items">
+      <a href=<?= "$dir/" ?> class=<?= $pagina == 'principal' ? 'active' : ''  ?>>Página inicial</a>
+      <a href=<?= "$dir/mais-buscados/" ?> class=<?= $pagina == 'mais-buscados' ? 'active' : ''  ?>>Mais buscados</a>
+      <a href=<?= "$dir/sugestoes" ?> class=<?= $pagina == 'sugestoes' ? 'active' : ''  ?>>Sugestões</a>
+      <a href=<?= "$dir/sobre" ?> class=<?= $pagina == 'sobre' ? 'active' : ''  ?>>Sobre</a>
+    </div>
+    <div class="login">
+      <a href=<?= "$dir/login" ?> class=<?= $pagina == 'login' ? 'active' : ''  ?>>
+        <p>Área</p>
+        <p>Restrita</p>
+      </a>
+    </div>
+    <div class="menu-hamburger">
+      <i class="fa-solid fa-bars"></i>
+      <div class="menu-hamburger-content">
+        <a href=<?= "$dir/" ?>>Página inicial</a>
+        <a href=<?= "$dir/mais-buscados" ?>>Mais buscados</a>
+        <a href=<?= "$dir/sugestoes" ?>>Sugestões</a>
+        <a href=<?= "$dir/sobre" ?>>Sobre</a>
+        <a href=<?= "$dir/login" ?>>Área Restrita</a>
+      </div>
+    </div>
+  </div>
+</header>
