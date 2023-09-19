5cac91c1233c99b3d2d7ae53a62004eadb6a79c5
rrafaelc <rrafaelc@users.noreply.github.com>
2023-09-19 10:52:52 +0000

    Prettified Code!

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const questionsContainer = document.querySelector('.container');
 const addLinksToContent = content => {
-  const linkRegex = /((http|https):\/\/[^\s.]+[^\s]*[^\s.])/g;  const linkReplacement = '<a href="$1" target="_blank" style="display: inline">clique aqui</a>';
+  const linkRegex = /((http|https):\/\/[^\s.]+[^\s]*[^\s.])/g;
+  const linkReplacement = '<a href="$1" target="_blank" style="display: inline">clique aqui</a>';
   return content.replace(linkRegex, linkReplacement);
 };
 const replaceLineBreaks = content => {
