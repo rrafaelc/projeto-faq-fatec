efe346a368f38479caaf8b1ff281b8c75b922103
rrafaelc <rrafaelc@users.noreply.github.com>
2023-11-11 22:57:00 +0000

    Prettified Code!

--- sistema/script.js
+++ sistema/script.js
@@ -32,4 +32,3 @@ const execute = async () => {
 };
 
 loggedIn && (await execute());
-
--- sugestoes/script.js
+++ sugestoes/script.js
@@ -4,38 +4,36 @@ const insertSuggestionsUrl = `${apiUrl}/pergunta-sugerida`;
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const inputName = form.querySelector('[name="nome"]')
-  const inputEmail = form.querySelector('[name="email"]')
-  const inputPhone = form.querySelector('[name="telefone"]')
-  const inputMessage = form.querySelector('[name="mensagem"]')
+  const inputName = form.querySelector('[name="nome"]');
+  const inputEmail = form.querySelector('[name="email"]');
+  const inputPhone = form.querySelector('[name="telefone"]');
+  const inputMessage = form.querySelector('[name="mensagem"]');
 
-  const userName = inputName.value
-  const userEmail = inputEmail.value
-  const userPhone = inputPhone.value
-  const userSuggestion = inputMessage.value
+  const userName = inputName.value;
+  const userEmail = inputEmail.value;
+  const userPhone = inputPhone.value;
+  const userSuggestion = inputMessage.value;
 
   const data = {
     nome: userName,
     pergunta: userSuggestion,
     email: userEmail,
-    telefone: userPhone
-  }
+    telefone: userPhone,
+  };
 
   const insertSuggestedQuestion = async () => {
-    try{
+    try {
       await fetch(insertSuggestionsUrl, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data)
-      })
-
-    }catch(err){
-      console.log(err)
+        body: JSON.stringify(data),
+      });
+    } catch (err) {
+      console.log(err);
     }
-    }
+  };
 
-    insertSuggestedQuestion()
-
-  })
+  insertSuggestedQuestion();
+});
